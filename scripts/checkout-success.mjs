5e455b7778f3c6b115a8233f4ff06427cea940c7
camiP89 <campug04041@stud.noroff.no>
2025-03-02 23:32:22 +0100

    Update checkout-success.mjs
    corrected typo

--- scripts/checkout-success.mjs
+++ scripts/checkout-success.mjs
@@ -3,5 +3,5 @@ let heading = document.querySelector("h1");
 heading.innerHTML = headingText;
 
 const messageContainer = document.getElementById('message-container');
-  messageContainer.textContent = "Thank you for you order. You will receive an update on the status of your order soon. We hope you enjoy your purchase and thank you for shopping with using. ORDER NUMBER: 153486";
+  messageContainer.textContent = "Thank you for you order. You will receive an update on the status of your order soon. We hope you enjoy your purchase and thank you for shopping with us. ORDER NUMBER: 153486";
   messageContainer.classList.add('message');
