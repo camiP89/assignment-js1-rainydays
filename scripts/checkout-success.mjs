64a8cc8321aee4f400858c85435991e4bd8399b4
camiP89 <campug04041@stud.noroff.no>
2025-03-02 20:37:14 +0100

    Added checkout success message to checkout-success.mjs
    Added checkout success message to checkout-success.mjs

--- scripts/checkout-success.mjs
+++ scripts/checkout-success.mjs
@@ -2,7 +2,6 @@ let headingText = "Checkout-Success";
 let heading = document.querySelector("h1");
 heading.innerHTML = headingText;
 
-const submitButton = document.createElement("button");
-submitButton.type = "submit";
-submitButton.textContent = "Submit Order";
-shippingForm.appendChild(submitButton);
+const messageContainer = document.getElementById('message-container');
+  messageContainer.textContent = "Thank you for you order. You will receive an update on the status of your order soon. We hope you enjoy your purchase and thank you for shopping with using. ORDER NUMBER: 153486";
+  messageContainer.classList.add('message');
